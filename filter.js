19b51200aa5881c9a578c8f2ffe523eb9598a35a
Jeffery Patton <jefferypatton@Jeff-Pattons-MacBook-Pro.local>
2018-09-11 12:21:23 -0400

    second break

--- filter.js
+++ filter.js
@@ -2,8 +2,8 @@
   Filter array method
     Gives you back an array of the same or less length
     Applies a filter to an array based on the condition within the callback
-    If the callback returns true; add the item to the new array
-    If the callback returns false; skip that item.
+    If the callback returns a truthy value; add the item to the new array
+    If the callback returns falsy value; skip that item.
 
     By default, filter does not modify the items.
 
@@ -26,7 +26,8 @@ const fruits = [
   'Pear',
   'Pineapple',
   'Plum',
-  'Strawberry'
+  'Strawberry',
+  ''
 ];
 // Create a new array where each item is fruits with less than 6 characters in their name
 
@@ -37,3 +38,58 @@ let shortFruits = fruits.filter(item => {
     return false;
   }
 });
+
+let shorterFruits = fruits.filter(fruit => fruit.length < 6); // Adds in empty strings because we don't check for them
+
+let shorterFruits = fruits.filter(fruit => {
+  if (!fruit) {
+    // Check for falsy values
+    return false;
+  } else {
+    return fruit.length < 6;
+  }
+});
+
+// Filter example: Count the number of vowels in a string
+function vowels(inputString) {
+  const vowelListStr = 'aeiou';
+
+  inputString = inputString.toLowerCase();
+
+  const stringArr = inputString.split('');
+
+  const vowels = stringArr.filter(char => vowelListStr.includes(char));
+  return vowels.length;
+}
+
+//Map example: Capitalize the first character in each word
+// Convert the following example to map
+function capitalize(sentence) {
+  // Get the individual words
+  let words = sentence.split(' ');
+
+  // Capitalize the first character in each word
+  words = words.map(word => word[0].toUpperCase() + word.slice(1));
+
+  return words.join(' ');
+}
+
+// Print only books that have more than 2 words in their name
+const bookArray = [
+  {
+    title: 'Harry Potter',
+    author: 'JK Rowling'
+  },
+  {
+    title: 'Name of the Wind',
+    author: 'Patrick Rothfuss'
+  },
+  {
+    title: 'Wheel of Time',
+    author: 'Robert Jordan'
+  },
+  {
+    title: 'A Song of Ice and Fire',
+    author: 'George RR Martin'
+  } //
+];
